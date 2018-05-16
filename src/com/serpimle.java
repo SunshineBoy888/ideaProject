318bec5ed5056b09ca6c625af9cd47cc5f8c1e2b
SunshineBoy888 <1241589616@qq.com>
2018-05-16 23:34:42 +0800

    this is my22

---
+++ src/com/serpimle.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2018/5/16 0016.
+ */
+public class serpimle<T> implements Serializable {
+
+    //实例化的时候要转变成具体类
+    public  T data;
+
+    //psvm 按一下（或者两下）键可以出现main函数
+    public static void main(String[] args) {
+
+        //sout 按一下（或者两下）键可以出现如下函数
+        System.out.println();
+
+    }
+
+
+}
