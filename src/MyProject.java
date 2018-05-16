d7a853de323ded3098005027180bd8abdbfb01c4
SunshineBoy888 <1241589616@qq.com>
2018-05-16 17:40:31 +0800

    this is my22

--- src/MyProject.java
+++ src/MyProject.java
@@ -1,11 +1,11 @@
 /**
  * Created by Administrator on 2018/5/16 0016.
+ * 如何你真的渴望做一件事，全世界都在帮你，如果你排斥做一件事，连空气都将成为你的阻碍
  */
 /**
  * 把git拿出来的步骤
  * 1、VCS->import into version control->create Git Repository
  */
-
 
 /**
  * idea上传以及更新代码步骤
