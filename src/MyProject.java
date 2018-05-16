ae52df358f529bad00ec24fab39dbe420dd7f827
SunshineBoy888 <1241589616@qq.com>
2018-05-16 17:32:47 +0800

    this is my22

--- src/MyProject.java
+++ src/MyProject.java
@@ -1,9 +1,18 @@
 /**
  * Created by Administrator on 2018/5/16 0016.
  */
+
+/**
+ * idea上传以及更新代码步骤
+ * 1、git->repository->fech
+ * 2、git->repository->pull
+ * 3、选择需要上传的代码
+ * 4、git->commit->directory
+ * 5、git->repository->push
+ */
+
 public class MyProject {
     public void index(){
-
         System.out.println("myProject");
     }
 }
